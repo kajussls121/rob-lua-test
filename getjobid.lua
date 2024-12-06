86d1e63b3ce28c9718da0c1381ae87d775a2f1e5
Kajus S <kajusslekys38@gmail.com>
2024-12-06 15:52:47 +0000

    Add getting job id of server, and modify loader

---
+++ getjobid.lua
@@ -0,0 +1,3 @@
+local jobId = game.JobId
+print("Current Job ID (Or server ID):", jobId)
+setclipboard(jobId)
--- loader.lua
+++ loader.lua
@@ -2,9 +2,9 @@ local urlroot = "https://raw.githubusercontent.com/kajussls121/rob-lua-test/refs
 local urlpath_lt2 = urlroot .. "/lt2.lua"
 local urlpath_bss = urlroot .. "/bss.lua"
 local urlpath_nds = urlroot .. "/nds.lua"
+local urlpath_getjobid = urlroot .. "/getjobid.lua"
 
 -- Function to load and execute a script
-
 local function loadAndExecuteScript(filepath)
     local scriptFunction, err = loadstring(game:HttpGet(filepath))
     if not scriptFunction then
@@ -13,7 +13,7 @@ local function loadAndExecuteScript(filepath)
         scriptFunction()
     end
 end
-
+loadAndExecuteScript(urlpath_getjobid)
 if game.PlaceId == 13822889 then
     loadAndExecuteScript(urlpath_lt2)
 end
