3269adc8547b2d1b79ec521f4e26e333cad3b87b
Kajus S <kajusslekys38@gmail.com>
2024-12-09 23:10:53 +0000

    Update LT2 to V2

--- getjobid.lua
+++ getjobid.lua
@@ -1,4 +1,8 @@
 local jobId = game.JobId
 print("Current Job ID (Or server ID):", jobId)
-setclipboard(jobId)
-print("Job ID is in Clipboard.")
+local success, errorMessage = pcall(setclipboard, jobId)
+if success then
+    print("Job ID is in clipboard.")
+else
+    print("Error setting clipboard: " .. errorMessage)
+end
--- lt2.lua
+++ lt2.lua
@@ -1,6 +1,7 @@
 -- Lumber Tycoon 2
-
-local kronHubScript, kronHubError = loadstring(game:HttpGet('https://raw.githubusercontent.com/DevKron/Kron_Hub/refs/heads/main/version_1.0'))
+--                                   v2 \/
+local kronHubScript, kronHubError = loadstring(game:HttpGet('https://raw.githubusercontent.com/DevKron/Kron_Hub/refs/heads/main/lt2new'))("")
+-- v1.9 > --loadstring(game:HttpGet('https://raw.githubusercontent.com/DevKron/Kron_Hub/refs/heads/main/version_1.0'))
 if kronHubError then
     print("Error loading Kron Hub script: " .. kronHubError)
 else
