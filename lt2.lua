7e7f297ec7f2501575592001a5725a0e062bd296
kajussls121 <82109610+kajussls121@users.noreply.github.com>
2025-03-27 20:40:43 +0000

    Update lt2.lua

--- lt2.lua
+++ lt2.lua
@@ -1,6 +1,6 @@
 -- Lumber Tycoon 2
 --                                   v2 \/
-local kronHubScript, kronHubError = loadstring(game:HttpGet('https://raw.githubusercontent.com/DevKron/Kron_Hub/refs/heads/main/lt2new'))("")
+local kronHubScript, kronHubError = loadstring(game:HttpGet('https://raw.githubusercontent.com/DevKron/Kron_Hub/refs/heads/main/version_1.0'))("")
 -- v1.9 > --loadstring(game:HttpGet('https://raw.githubusercontent.com/DevKron/Kron_Hub/refs/heads/main/version_1.0'))
 if kronHubError then
     print("Error loading Kron Hub script: " .. kronHubError)
